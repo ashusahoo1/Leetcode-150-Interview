1f402d8cef44df7227be82fc396ee8067ebe0a36
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-11-25 06:52:59 +0530

    Create 189.cpp

---
+++ 189.cpp
@@ -0,0 +1,19 @@
+//189. Rotate Array
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        vector<int> rotated(n);
+
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotated[i];
+        }        
+    }
+};
